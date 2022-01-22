cd6baf65bc265ae6e91be45f4bc9a55d13563581
KevOneRedOne <kevinalves@hotmail.fr>
2022-01-22 10:08:52 +0100

    Update front Login/register - Clement

--- php_exam/login.php
+++ php_exam/login.php
@@ -1,70 +1,39 @@
 <!DOCTYPE html>
-<html lang="FR">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projet Forum</title>
-    <!-- <link rel="icon" href="/static/img/icon/forum.png"> -->
-    <link rel="stylesheet" href="assets/css/login.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
-</head>
-<body>
-    <header>
-        <nav class="navbar">
-            <div id="forum_tittle">
-                <a href="/php_exam/home.php">Forum</a>  
-            </div>
-        </nav>
-    </header>
-    <div class="connexion">
-        <div class="L-Title flex padding">
-            <h1>Connexion</h1>
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/css/login.css">
+        <title>Login</title>
+    </head>
+    <body>
+        <div class="Forum">
+            <a>Forum</a>
         </div>
-        <div class="loginForm padding flex">
-            <form action="" method="POST">
-                <div id="mail"> 
-                    <br>
-                    <label for="mail"> E-mail : </label>
-                    <br>
-                    <input type="email" id="mail" name="user_mail" required>
-                </div>
-                <div id="mdp">
-                    <label for="password"> Mot de passe : </label>
-                    <br>
-                    <input type="password" id="password" name="user_password" required>
-                </div>
-                <div class="btnLogin flex">
-                    <button class="LOGIN" type="submit">Connexion</button>
-                </div>
-            </form>
-        </div>
-
-        <div class="otherLogin padding flex">
-            <div class="O-inscription">
-                <p>Vous n'avez pas de compte ?</p>
-                <p>Incrivez-vous ici</p>
-                <br>
-                <div class="flex">
-                    <a class="REGISTER" href="/php_exam/register.php">Inscription</a>
+        <form method="POST" action="">
+            <div class="container">
+                <div class="margcont">
+                    <h1>S'identifier</h1>
+                    <div id="mail">
+                        <a>Adresse e-mail</a>
+                        <input type="email" id="mail" name="user_mail" required>
+                    </div>
+                    <div id="mdp">
+                        <a>Mot de passe</a>
+                        <input type="password" id="password" name="user_password" required>
+                    </div>
+                    <div class="input-group">
+                        <button class="login" type="submit">Connexion</button>
+                    </div>
+                    <div class="text">
+                        <a>Pas encore membre ?</a>
+                        <a class="register" href="register.php">Créer un compte</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-</body>
+        </form>
+    </body>
 </html>
 
 <?php
     include("assets/php/loginDB.php");
     loginDB();
-    $result = $mysqli->query("SELECT USERNAME, MAIL FROM users"); // On utilise l'instance créée pour faire une requête bidon
-    $nb_users = mysqli_num_rows($result);
-    while ($data = mysqli_fetch_array($result)){
-        echo htmlentities(trim($data['USERNAME']));
-        echo htmlentities(trim($data['MAIL']));
-    } 
-    
-    
-  
 ?>
