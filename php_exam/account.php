a72d571b5a2618dc5575d3048adfda12187a7005
KevOneRedOne <kevinalves@hotmail.fr>
2022-01-23 17:32:44 +0100

    MAJ Account.php - Timothy

--- php_exam/account.php
+++ php_exam/account.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <link rel="stylesheet" href="assets/css/account.css">
-        <title>Home</title>
+        <title>Account</title>
     </head>
     <body>
         <form action="" method="POST">
@@ -17,13 +17,15 @@
                                 exit(); 
                             }
                             elseif($_SESSION['username'] !== ""){
+                                global $user;
                                 $user = $_SESSION['username'];
                                 echo "<a>Bonjour $user, vous êtes connectés</a>";
                             }
+                            include("logoutDB.php");
                         ?>  
                         <a href="new.php">+</a>
                         <a href="login.php" onclick="logOut();">Déconnexion</a>
-                        <a href="account.php">Compte</a>
+                        <a href="account.php">$user</a>
                         <a href="admin.php">ADMIN</a>
                     </div>
                 </div>
@@ -31,10 +33,12 @@
             <div class="container">
                 <div class="margcont">
                     <div class="account">
-                        <a>Username</a>
+                        <a>$_SESSION['username']</a>
                     </div>
                     <div class="inputgroup">
                         <a>Changer d'addresse e-mail</a><br>
+                            
+
                         <input type="email" placeholder="email" name="email">
                         <button type="submit" name="email">Changer</button>
                     </div>
@@ -49,12 +53,26 @@
                         </div>
                         <div class="articles">
                             <a href="details.php">
-                                <a class="username">Username</a>
-                                <a class="date">01/01/2022</a>
-                                <div class="description">
-                                    <a>description</a>
-                                </div>
-                                <img src="" alt="">
+
+                        <?php
+                        // include("loginDB.php");
+                        // loginDB();
+                        // $idPost = $_GET['id'];
+                        // $requete = "SELECT * FROM users.USERNAME WHERE ='".$user."'";
+                        // $exec = mysqli_query($mysqli,$requete);
+                        // $rep = mysqli_fetch_array($exec);
+
+
+                        
+                        // $stmt = $mysqli->prepare("UPDATE INTO users(MAIL) VALUES ('$email')");
+                        ?>
+
+                            <p><?php echo $rep1['USERNAME'];?></p>
+                            <a class="date">01/01/2022</a>
+                            <div class="description">
+                                <a>description</a>
+                            </div>
+                            <img src="" alt="">
                             </a>
                             <div class="divider"></div>
                         </div>
@@ -66,5 +84,5 @@
 </html>
 
 <?php
-    include("logoutDB.php");
+
 ?>
