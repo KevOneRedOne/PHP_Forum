c627ab8554942252d41dbc7baf65f5d1401c83b2
deoko <deoko@outlook.fr>
2022-01-23 14:11:15 +0100

    push account correction

--- php_exam/account.php
+++ php_exam/account.php
@@ -9,47 +9,10 @@ if(isset($_GET['id']) AND $_GET['id'] > 0)
 ?> 
 
 <!DOCTYPE html>
-<<<<<<< HEAD
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Projet Forum</title>
-    <link rel="stylesheet" href="assets/css/register.css">
-    <link rel="preconnect" href="https://fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css2?family=Lobster&display=swap" rel="stylesheet">
-</head>
-<body>
-    <header>
-        <nav class="navbar">
-            <div id="forum_tittle">
-                <a href="/php_exam/home.php">Forum</a>  
-            </div>
-        </nav>
-    </header>
-
-    <div class="account">
-        <div class="L-Title flex padding">
-            <h2>Votre compte</h2>
-        <div align="center">
-            <h1>profil de <?php echo $userinfo['USERNAME']; ?></h1>
-            <br />
-            <h1>mail :<?php echo $userinfo['MAIL']; ?></h1>
-            <br />
-            <a href="modprofil.php">modifier mon profil</a>
-            <br />
-            <a href="deco.php">Deconnexion</a>
-            
-</body>
-</html>
-
-
-=======
 <html>
     <head>
-        <link rel="stylesheet" href="assets/css/account.css">
-        <title>Home</title>
+        <link rel="stylesheet" href="assets/css/details.css">
+        <title>Details</title>
     </head>
     <body>
         <form action="" method="POST">
@@ -72,47 +35,30 @@ if(isset($_GET['id']) AND $_GET['id'] > 0)
                         <a href="login.php" onclick="logOut();">Déconnexion</a>
                         <a href="account.php">Compte</a>
                         <a href="admin.php">ADMIN</a>
+                        
                     </div>
                 </div>
             </div>
-            <div class="container">
-                <div class="margcont">
-                    <div class="account">
-                        <a>Username</a>
-                    </div>
-                    <div class="inputgroup">
-                        <a>Changer d'addresse e-mail</a><br>
-                        <input type="email" placeholder="email" name="email">
-                        <button type="submit" name="email">Changer</button>
-                    </div>
-                    <div class="inputgroup">
-                        <a>Changer de mot de passe</a><br>
-                        <input type="password" placeholder="mot de passe" name="psw">
-                        <button type="submit" name="email">Changer</button>
-                    </div>
-                    <div>
-                        <div class="publication">
-                            <a>Publications</a>
-                        </div>
-                        <div class="articles">
-                            <a href="details.php">
-                                <a class="username">Username</a>
-                                <a class="date">01/01/2022</a>
-                                <div class="description">
-                                    <a>description</a>
-                                </div>
-                                <img src="" alt="">
-                            </a>
-                            <div class="divider"></div>
-                        </div>
-                    </div>
-                </div>
+
+            <div id="forum_tittle">
+                <a href="/php_exam/home.php">Forum</a>  
             </div>
-        </form>
-    </body>
-</html>
+        </nav>
+    </header>
+
+    <div class="account">
+        <div class="L-Title flex padding">
+            <h2>Votre compte</h2>
+        <div align="center">
+            <h1>profil de <?php echo $userinfo['USERNAME']; ?></h1>
+            <br />
+            <h1>mail :<?php echo $userinfo['MAIL']; ?></h1>
+            <br />
+            <a href="modprofil.php">modifier mon profil</a>
+            <br />
+            <a href="login.php">Deconnexion</a>
+ 
 
 <?php
     include("logoutDB.php");
 ?>
->>>>>>> e604d5e7145e8634f386dfbdac51ba3f040881f2
