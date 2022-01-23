58864a898dd78ad2e57a90d29d356551e4ee395a
CLANNERETONNE <72856302+CLANNERETONNE@users.noreply.github.com>
2022-01-23 14:15:43 +0100

    edit.php back-end

--- php_exam/edit.php
+++ php_exam/edit.php
@@ -1,3 +1,8 @@
+<?php
+    $mysqli = new mysqli("localhost", "root", "", "php_exam_db");
+    global $urlid;
+    $urlid = $_GET['id'];
+?>
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,7 +10,7 @@
         <title>Edit</title>
     </head>
     <body>
-        <form action="" method="POST">
+        <form method="POST" action="">
             <div class="navbar">
                 <div class="nav-cont">
                     <a href="home.php">Forum</a>
@@ -17,34 +22,47 @@
                                 exit(); 
                             }
                             elseif($_SESSION['username'] !== ""){
+                                global $user;
                                 $user = $_SESSION['username'];
-                                echo "<a>Bonjour $user, vous êtes connectés</a>";
+                                echo "<a>Bonjour ".$user.", vous êtes connectés</a>";
                             }
-                        ?>  
+                            include("logoutDB.php");
+                        ?>    
                         <a href="new.php">+</a>
                         <a href="login.php" onclick="logOut();">Déconnexion</a>
-                        <a href="account.php">Compte</a>
-                        <a href="admin.php">ADMIN</a>
+                        <a href="account.php"><?php echo $user; ?></a>
                     </div>
                 </div>
             </div>
             <div class="container">
                 <div class="articles">
-                    <a class="username">Username</a>
-                    <a class="date">01/01/2022</a>
-                    <textarea style="resize: none;">description</textarea>
-                    <div class="img-container">
-                        <img src="" alt="">
-                        <div class="btn-cont">
-                            <button class="delete">supprimer</button>
-                            <button class="save">enregistrer</button>
-                        </div>
+                    <a class="username"><?php echo $username; ?></a>
+                    <?php       
+                        $requete2 = "SELECT TITLE, DATE_CREATION, DESCRIPTION FROM articles WHERE ID = '".$urlid."';";
+                        $exec2 = mysqli_query($mysqli, $requete2);
+                        while($rep = mysqli_fetch_array($exec2)){
+                    ?>
+                    <a class="date"><?php echo $rep['DATE_CREATION']?></a>
+                    <input class="titre" id="titre" name='titre' placeholder="<?php echo $rep['TITLE']?>"></input>
+                    <textarea style="resize: none;" placeholder="<?php echo $rep['DESCRIPTION']?>" name="description" id ="description"></textarea>
+                    <?php }; //close while loop ?>
+                    <div class="btn-cont">
+                        <button class="save" type="submit" name="save" >Enregistrer</button>
                     </div>
                 </div>
             </div>
+            <?php
+                
+                $titre = $_POST['titre'];
+                $description = $_POST['description'];
+                $urlid = $_GET['id'];
+
+                $stmt = $mysqli->prepare("UPDATE Articles SET TITLE = '".$titre."', DESCRIPTION = '".$description."',  WHERE ID='".$urlid."';");
+                echo $urlid;
+                echo $titre;
+                echo $description;
+                $stmt->execute();
+            ?>
         </form>
     </body>
 </html>
-<?php
-    include("logoutDB.php");
-?>
