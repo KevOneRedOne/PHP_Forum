79ac8c121063519f838f05513de11aa3d433fc54
KevOneRedOne <kevinalves@hotmail.fr>
2022-01-23 14:36:34 +0100

    Reparation git

--- php_exam/admin.php
+++ php_exam/admin.php
@@ -66,6 +66,6 @@
         } else {
             header('Location: admin.php?erreur=2'); // utilisateur ou mot de passe vide
         }
-        mysqli_close($db);
+        mysqli_close($db); //close db
     }
 ?>
